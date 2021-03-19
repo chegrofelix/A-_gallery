91eb2ddf097b3a9bf2c082018be89a9488efbc42
chegrofelix <chegerofelix@gmail.com>
2021-03-19 23:45:17 +0300

    added code to views.py in gallery app and created urls.py in gallery app

--- A_gallery/gallery/views.py
+++ A_gallery/gallery/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse('<h1> A+ GALLERY HOME</h1>')
+
+def about(request):
+    return HttpResponse('<h1> A+ GALLERY ABOUT</h1>')

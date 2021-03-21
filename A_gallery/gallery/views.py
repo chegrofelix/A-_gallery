95f2831897d2d42055f12df06cae774469f99c6c
chegrofelix <chegerofelix@gmail.com>
2021-03-21 15:42:34 +0300

    modified views.py and seetings.py

--- A_gallery/gallery/views.py
+++ A_gallery/gallery/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1> A+ GALLERY HOME</h1>')
+    return render(request, 'gallery/home.html')
 
 def about(request):
-    return HttpResponse('<h1> A+ GALLERY ABOUT</h1>')
+    return render(request, 'gallery/about.html')

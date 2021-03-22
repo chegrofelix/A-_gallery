8e5c80a24301f886ca0a6b19b307431ec570713f
chegrofelix <chegerofelix@gmail.com>
2021-03-22 07:05:22 +0300

    created new file templates

--- A_gallery/gallery/urls.py
+++ A_gallery/gallery/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='galley-home'),
-    path('', views.about, name='galley-about'),
+    path('', views.home, name='gallery-home'),
+    path('', views.about, name='gallery-about'),
 ]

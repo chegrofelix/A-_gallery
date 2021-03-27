6ef72ab063f8763c369d0962511d7749fbc2d020
chegrofelix <chegerofelix@gmail.com>
2021-03-28 00:36:13 +0300

    changed allowed hosts to any

--- A_gallery/settings.py
+++ A_gallery/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', 'gallery-fox.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
